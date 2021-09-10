c3d3b87b66fac739d71ec639d43e428b6f0b3070
Maria <maria.mihaila.17@gmail.com>
2021-09-10 15:06:55 +0300

    Create route that gets the medicalHistory file of a person

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyParser from 'express';
+
+const app = express();
+
+const port = 3000;
+
+app.use(bodyParser.json());
+
+
+app.get('/person/:personId/medicalHistory', (req, res) => {
+    const personId = req.params.personId;
+
+    const response = {
+        personId,
+        firstName: "Maria",
+        lastName: "Mihaila",
+        medicalHistory: {
+            diseases: 'none',
+            age: '26',
+            sex: 'f',
+            reasonForCheckup: 'headaches'
+        }
+    };
+
+    res.send(response);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
