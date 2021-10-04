7d85ca3a2e777b383dd183f5ac10744e70f0049b
Maria <maria.mihaila.17@gmail.com>
2021-10-04 17:26:17 +0300

    Refactor js files to ts

---
+++ src/create-web-server.ts
@@ -0,0 +1,105 @@
+import * as http from 'http';
+import * as express from "express";
+import * as bodyParser from 'express';
+
+
+export const createWebServer = () => {
+    const app = express();
+
+    const port = 3000;
+
+    app.use(bodyParser.json());
+
+    app.get('/history/:userId', (req, res) => {
+        if (!req.params.userId) throw new Error('The `userId` parameter is not present.');
+
+        const userId = req.params.userId;
+
+        const response = {
+            userId,
+            entries: [{
+                childhoodDisease: 'bkdjad',
+                majorAdultDisease:'rhsh',
+                surgeries: 'dfhsdh',
+                priorInjuries: 'dfhdfh',
+                medications: 'sdhd',
+                allergies: 'dhdfh'
+            }]
+        };
+
+        res.send(response);
+    });
+
+    app.post('/history/create', (req, res) => {
+        // temporary payload format
+
+        const payload = {
+            userId: 12,
+            childhoodDisease: 'bkdjad',
+            majorAdultDisease:'rhsh',
+            surgeries: 'dfhsdh',
+            priorInjuries: 'dfhdfh',
+            medications: 'sdhd',
+            allergies: 'dhdfh'
+        }
+
+        const response = {
+            status: "ok",
+            message: "New medical History created."
+        }
+        res.send(response);
+    });
+
+    app.put('/history/:userId/edit', (req, res) => {
+
+        // temporary payload format
+        const payload = {
+            userId: 12,
+            childhoodDisease: 'bkdjad',
+            majorAdultDisease:'rhsh',
+            surgeries: 'dfhsdh',
+            priorInjuries: 'dfhdfh',
+            medications: 'sdhd',
+            allergies: 'dhdfh'
+        }
+
+        const response = {
+            status: "ok",
+            message: "Medical history updated."
+        }
+        res.send(response);
+    });
+
+    app.delete('/history/:userId/delete', (req, res) => {
+
+        res.send(`The user with the id ${req.params.userId} was deleted from the database.`);
+    })
+
+    const server = http.createServer(app);
+
+    const start = () => {
+        return new Promise<void>((resolve, reject) => {
+            server.listen(port, () => {
+                console.log(`App listening at http://localhost:${port}`);
+                resolve();
+            });
+        });
+    }
+
+    const stop = () => {
+        return new Promise<void>((resolve, reject) => {
+            server.close(err => {
+                if (err) return reject();
+
+                console.log(`App closed from http://localhost:${port}`);
+                resolve();
+            })
+        });
+    }
+
+    return {
+        start,
+        stop,
+        port
+    }
+}
