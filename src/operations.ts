b3655fab13c2539ae8df150422a8a3992e0bbd02
Maria <maria.mihaila.17@gmail.com>
2021-10-05 18:25:53 +0300

    Add fecthRecords function to the operation file

--- src/operations.ts
+++ src/operations.ts
@@ -17,3 +17,24 @@ export type MedicalOperation = {
     allergies: string,
 }
 
+/**
+ * Fetches records from the table and maps them to DTO (data transfer objects).
+ * @param pool
+ * @param userId
+ */
+export const fetchRecords = async (pool: Pool, userId: number) => {
+    const {rows} = await pool.query(`SELECT * FROM hbb_health.records WHERE user_id=${userId}`);
+
+    if (rows[0] === undefined) throw new Error(`The records with the id: ${userId} do not exist.`);
+
+    return {
+        id: rows[0].id,
+        userId: rows[0].user_id,
+        childhoodDisease: rows[0].childhood_disease,
+        majorAdultDisease: rows[0].major_adult_disease,
+        surgeries: rows[0].surgeries,
+        priorInjuries: rows[0].prior_injuries,
+        medications: rows[0].medications,
+        allergies: rows[0].allergies,
+    }
+}
