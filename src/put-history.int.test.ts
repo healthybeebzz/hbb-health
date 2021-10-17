fb2d681fc6c5791e87c23b4fc152500774222c8e
Maria <maria.mihaila.17@gmail.com>
2021-10-17 23:04:35 +0300

    Fix put-history int test

--- src/put-history.int.test.ts
+++ src/put-history.int.test.ts
@@ -1,44 +1,61 @@
 import {expect} from 'chai';
-import * as request from 'supertest';
 import {createWebServer} from "./create-web-server";
+import {default as axios} from 'axios';
+import {connectToDb} from "./db-connect";
+import {insertOperation} from "./operations";
 
-
-describe('/history/create', () => {
+describe('/history/1/edit', () => {
     let port: number;
     let server: { stop: () => Promise<unknown>, port: number, start: () => Promise<unknown> };
+
+    let pool = connectToDb();
 
     before(async () => {
         server = createWebServer();
         port = server.port;
 
         await server.start();
-    });
 
-    after(async () => {
-        await server.stop();
-    });
+        await pool.query(`ALTER SEQUENCE hbb_health.records_id_seq RESTART`);
+        await pool.query(`ALTER SEQUENCE hbb_health.records_user_id_seq RESTART`);
 
-    it('given not existing entry > when calling post /history/create > should return valid response', async () => {
-        const payload = {
+        await insertOperation(pool, {
             childhoodDisease: "disease big",
             majorAdultDisease: "chickpot",
             surgeries: "brain surgery",
             priorInjuries: "head injury",
             medications: "xannax",
             allergies: "all allergies"
+        });
+    });
+
+    after(async () => {
+        await pool.query(`
+            DELETE FROM hbb_health.records WHERE user_id=1`);
+
+        await server.stop();
+    });
+
+    it('given not existing entry > when calling post /history/create > should return valid response', async () => {
+        const payload = {
+            childhoodDisease: "chickenpox"
         };
 
-        const response = await request(`http://localhost:${port}`).post('/history/create').send(payload);
+        const response = await axios.put(`http://localhost:${port}/history/1/edit`, payload);
 
         expect(response.status).to.be.equal(200);
-        expect(response.body).to.be.deep.equal({
-            "entries": [
-                {
-                    "date": "12.12.2012",
-                    "info": "Bla bla",
-                }
-            ],
-            "personId": "1"
+        expect(response.data).to.be.deep.equal({
+            patientDetails: {
+                allergies: 'all allergies',
+                childhoodDisease: 'chickenpox',
+                id: '1',
+                majorAdultDisease: 'chickpot',
+                medications: 'xannax',
+                priorInjuries: 'head injury',
+                surgeries: 'brain surgery',
+                userId: 1
+            },
+            userId: "1"
         });
     });
 });
