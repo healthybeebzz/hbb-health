cbe41544530022e52aec7282556295ade9c050b2
Maria <maria.mihaila.17@gmail.com>
2021-10-17 22:30:41 +0300

    Fix get history int test

--- src/get-medical-history.int.test.ts
+++ src/get-medical-history.int.test.ts
@@ -1,41 +1,59 @@
 import {expect} from 'chai';
-import request from 'supertest';
+import {default as axios} from 'axios';
 import {createWebServer} from "./create-web-server";
+import {connectToDb} from "./db-connect";
+import {insertOperation} from "./operations";
 
 
-describe('/person/:personId/medical-history', () => {
+describe('/history/1', () => {
     let port: number;
     let server: { stop: () => Promise<unknown>, port: number, start: () => Promise<unknown> };
+
+    let pool = connectToDb();
 
     before(async () => {
         server = createWebServer();
         port = server.port;
 
         await server.start();
+
+        await pool.query(`ALTER SEQUENCE hbb_health.records_id_seq RESTART `);
+        await pool.query(`ALTER SEQUENCE hbb_health.records_user_id_seq RESTART `);
+
+        await insertOperation(pool, {
+            childhoodDisease: 'disease number 1',
+            majorAdultDisease: 'no disease',
+            surgeries: 'eye surgery',
+            priorInjuries: 'liver injury',
+            medications: 'xanax',
+            allergies: 'bees'
+        });
     });
 
     after(async () => {
+        await pool.query(`
+            DELETE FROM hbb_health.records WHERE user_id=1`);
+
         await server.stop();
     });
 
-    it('given existing entries > when calling get /person/:personId/medical-history > should return valid response', async () => {
-        const payload = {
-            requestId: 1,
-            username: 'test123',
-            externalToken: 1
-        };
+    it('given existing entries > when calling get /history/:userId > should return valid response', async () => {
 
-        const response = await request(`http://localhost:${port}`).get('/person/1/medical-history').send(payload);
+        const response = await axios.get(`http://localhost:${port}/history/1`);
 
         expect(response.status).to.be.equal(200);
-        expect(response.body).to.be.deep.equal({
-            "entries": [
-                {
-                    "date": "12.12.2012",
-                    "info": "Bla bla",
-                }
-            ],
-            "personId": "1"
+        expect(response.data).to.be.deep.equal({
+                records: {
+                    childhoodDisease: 'disease number 1',
+                    majorAdultDisease: 'no disease',
+                    surgeries: 'eye surgery',
+                    id: "1",
+                    priorInjuries: 'liver injury',
+                    medications: 'xanax',
+                    allergies: 'bees',
+                    userId: 1
+                },
+                userId: "1"
+            });
         });
-    });
 });
